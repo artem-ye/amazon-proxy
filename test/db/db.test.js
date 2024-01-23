4a290093c14c4c882c7ce53a3bfcf7996b30de5e
ay <shelmafona@gmail.com>
2024-01-23 20:34:54 +0200

    add MongoMemoryServer

--- test/db/db.test.js
+++ test/db/db.test.js
@@ -1,8 +1,17 @@
 const { connect, close } = require('../../src/core/db/db');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const mongod = MongoMemoryServer.create();
+
+afterAll(async () => {
+	(await mongod).stop();
+});
 
 it('DB connecting without err', async () => {
+	const uri = await (await mongod).getUri();
+
 	try {
-		await connect();
+		await connect(uri);
 		await close();
 	} catch (err) {
 		expect(err).toEqual('error');
