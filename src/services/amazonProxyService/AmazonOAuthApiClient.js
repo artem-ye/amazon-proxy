113e3f92a4f24eed9cbf99c8ea040bd143c74fc4
ay <shelmafona@gmail.com>
2024-01-25 21:15:01 +0200

    add AmazonOAuthApiClient class

--- src/services/amazonProxyService/AmazonOAuthApiClient.js
+++ src/services/amazonProxyService/AmazonOAuthApiClient.js
@@ -1,16 +1,84 @@
 const { default: axios } = require('axios');
 
-const AUTH_ENDPOINT = 'https://api.amazon.com/auth/o2/token';
+const OAUTH_ENDPOINT = 'https://api.amazon.com/auth/o2/token';
 
 class AmazonOAuthApiClient {
 	constructor(options = {}) {
 		this.client = options.client || axios.create();
-		this.api_endpoint = options.api_endpoint || AUTH_ENDPOINT;
+		this.api_endpoint = options.api_endpoint || OAUTH_ENDPOINT;
+		const { getCredentials, saveTokens } = options;
+		this.db = { getCredentials, saveTokens };
+		// this.refreshRequest = null;
 	}
 
-	async requestTokens({ client_id, client_secret }) {}
+	async login() {
+		const { client_id, client_secret } = await this.db.getCredentials();
+		this.#validateOauthCredentials({ client_id, client_secret });
 
-	async refreshTokens({ refresh_token }) {}
+		const params = {
+			grant_type: 'client_credentials',
+			scope: 'sellingpartnerapi::notifications',
+			client_id,
+			client_secret,
+		};
+		const data = await this.#oauthRequest(params);
+		await this.db.saveTokens(data);
+		return data;
+	}
+
+	async refreshToken() {
+		const { client_id, client_secret, refresh_token } =
+			await this.db.getCredentials();
+		this.#validateOauthCredentials({ client_id, client_secret });
+		this.#validateOAuthRefreshToken({ refresh_token });
+
+		const params = {
+			grant_type: 'refresh_token',
+			client_id,
+			client_secret,
+			refresh_token,
+		};
+		const data = await this.#oauthRequest(params);
+		await this.db.saveTokens(data);
+		return data;
+	}
+
+	async #oauthRequest(params) {
+		const reqConfig = {
+			method: 'post',
+			maxBodyLength: Infinity,
+			url: this.api_endpoint,
+			headers: {
+				'Content-Type':
+					'application/x-www-form-urlencoded;charset=UTF-8',
+			},
+			params,
+		};
+
+		const { data, statusText, status } = await this.client.request(
+			reqConfig
+		);
+
+		if (status !== 200) {
+			throw new Error(JSON.stringify({ status, statusText, data }));
+		}
+
+		return data;
+	}
+
+	#validateOauthCredentials({ client_id, client_secret }) {
+		if (!client_id || !client_secret) {
+			throw new Error(
+				`Invalid credentials client_id: ${client_id} client_secret; ${client_secret}`
+			);
+		}
+	}
+
+	#validateOAuthRefreshToken({ refresh_token }) {
+		if (!refresh_token) {
+			throw new Error(`Invalid refresh_token ${refresh_token}`);
+		}
+	}
 }
 
 module.exports = AmazonOAuthApiClient;
---
+++ test/amazonProxy/amazonOAuthApiClient.test.js
@@ -0,0 +1,87 @@
+const AmazonOAuthApiClient = require('../../src/services/amazonProxyService/AmazonOAuthApiClient');
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+const API_ENDPOINT = '/';
+let api;
+let mockClient;
+let mockCredentials;
+
+beforeEach(() => {
+	const client = axios.create();
+	mockClient = new MockAdapter(client);
+
+	mockCredentials = {
+		client_id: 'auth_cl_id',
+		client_secret: 'auth_cl_secret',
+		access_token: 'auth_access_token',
+		refresh_token: 'auth_refresh_token',
+	};
+
+	const options = {
+		api_endpoint: API_ENDPOINT,
+		client,
+		getCredentials: () => mockCredentials,
+		saveTokens: (data) => {
+			Object.assign(mockCredentials, { ...data });
+		},
+	};
+	api = new AmazonOAuthApiClient(options);
+});
+
+describe('login', () => {
+	it('correct login request should works', async () => {
+		const { client_id, client_secret } = mockCredentials;
+		const LOGIN_REQUEST_PARAMS = {
+			grant_type: 'client_credentials',
+			scope: 'sellingpartnerapi::notifications',
+			client_id,
+			client_secret,
+		};
+
+		const LOGIN_RESPONSE_BODY = {
+			access_token: 'new_auth_access_token',
+			refresh_token: 'new_auth_refresh_token',
+			token_type: 'bearer',
+			expires_in: 3600,
+		};
+
+		mockClient.onPost(API_ENDPOINT).reply(200, LOGIN_RESPONSE_BODY);
+
+		await api.login();
+
+		expect(mockClient.history.post[0].params).toMatchObject(
+			LOGIN_REQUEST_PARAMS
+		);
+		expect(mockCredentials).toMatchObject(LOGIN_RESPONSE_BODY);
+	});
+
+	it.todo('incorrect login request should fail');
+});
+
+describe('refreshToken', () => {
+	it('correct refreshToken request should works', async () => {
+		const { client_id, client_secret, refresh_token } = mockCredentials;
+		const REQUEST_PARAMS = {
+			grant_type: 'refresh_token',
+			client_id,
+			client_secret,
+			refresh_token,
+		};
+
+		const RESPONSE_BODY = {
+			access_token: 'refresh_auth_access_token',
+			refresh_token: 'refresh_auth_refresh_token',
+			token_type: 'bearer',
+			expires_in: 3600,
+		};
+
+		mockClient.onPost(API_ENDPOINT).reply(200, RESPONSE_BODY);
+		await api.refreshToken();
+
+		expect(mockClient.history.post[0].params).toMatchObject(REQUEST_PARAMS);
+		expect(mockCredentials).toMatchObject(RESPONSE_BODY);
+	});
+
+	it.todo('correct refreshToken request should fil');
+});
