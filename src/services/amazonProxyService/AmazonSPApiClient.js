fd6eca1eac450287fc53797bef70afd0e4dcefb1
ay <shelmafona@gmail.com>
2024-01-26 15:54:02 +0200

    add AmazonSPApiClient class

--- src/services/amazonProxyService/AmazonSPApiClient.js
+++ src/services/amazonProxyService/AmazonSPApiClient.js
@@ -1,4 +1,4 @@
-const { default: axios } = require('axios');
+const { default: axios, AxiosError } = require('axios');
 
 const API_ENDPOINT = 'https://sellingpartnerapi-eu.amazon.com';
 
@@ -6,11 +6,78 @@ class AmazonSPApiClient {
 	constructor(options = {}) {
 		this.client = options.client || axios.create();
 		this.api_endpoint = options.api_endpoint || API_ENDPOINT;
+
+		this.authService = options.authService;
+		this.#validateAuthService();
+
+		this.client.interceptors.request.use(
+			async (config) => {
+				const { access_token } =
+					await this.authService.getCredentials();
+				if (!access_token) {
+					return config;
+				}
+
+				const newConfig = {
+					headers: {},
+					...config,
+				};
+
+				newConfig.headers['x-amz-access-token'] = access_token;
+				return newConfig;
+			},
+			(err) => Promise.reject(err)
+		);
+
+		this.client.interceptors.response.use(
+			(res) => res,
+			async (err) => {
+				if (
+					!(err instanceof AxiosError) ||
+					err.response.status !== 403 ||
+					err.config.retry
+				) {
+					throw err;
+				}
+
+				await this.authService.refreshToken();
+				const newConfig = {
+					...err.config,
+					retry: true,
+				};
+				return this.client(newConfig);
+			}
+		);
 	}
 
-	async request() {}
+	async request(request) {
+		const { url, method, headers, body: data } = request;
+
+		const reqConfig = {
+			baseURL: this.api_endpoint,
+			url,
+			method,
+			data,
+			headers,
+		};
+
+		return await this.client.request(reqConfig);
+	}
+
+	async login() {
+		return this.authService.login();
+	}
+
+	#validateAuthService() {
+		if (typeof this.authService.getCredentials !== 'function') {
+			throw new Error(
+				'options.authService.getCredentials must be a function'
+			);
+		}
+		if (typeof this.authService.login !== 'function') {
+			throw new Error('options.authService.login must be a function');
+		}
+	}
 }
 
-module.exports = {
-	AmazonSPApiClient,
-};
+module.exports = AmazonSPApiClient;
---
+++ test/amazonProxy/amazonSPApiClinet.test.js
@@ -0,0 +1,155 @@
+const { default: axios, AxiosError } = require('axios');
+const { default: AxiosMockAdapter } = require('axios-mock-adapter');
+const AmazonSPApiClient = require('../../src/services/amazonProxyService/AmazonSPApiClient');
+
+const API_ENDPOINT = '/api';
+const TOKEN_EXPIRED_STATUS = 403;
+
+let authService;
+let mockClient;
+let api;
+
+beforeEach(() => {
+	authService = {
+		getCredentials: async () => {},
+		login: async () => {},
+		refreshToken: async () => {},
+	};
+
+	const client = axios.create();
+	mockClient = new AxiosMockAdapter(client);
+
+	api = new AmazonSPApiClient({
+		client,
+		authService,
+		api_endpoint: API_ENDPOINT,
+	});
+});
+
+it('login stub works', async () => {
+	authService.login = async () => {
+		Promise.resolve();
+		return 'hello';
+	};
+
+	const res = await api.login();
+	expect(res).toMatch('hello');
+});
+
+describe('request', () => {
+	it('request sends correct data', async () => {
+		authService.getCredentials = async () => {
+			Promise.resolve();
+			return {
+				access_token: 'token',
+			};
+		};
+
+		const POST_DATA = { hello: 'world' };
+		const RESPONSE_DATA = { hi: 'world' };
+
+		mockClient
+			.onPost(
+				`${API_ENDPOINT}/foo`,
+				POST_DATA,
+				expect.objectContaining({
+					foo: 'bar',
+					['x-amz-access-token']: 'token',
+				})
+			)
+			.reply(200, RESPONSE_DATA);
+
+		const { data, status } = await api.request({
+			method: 'post',
+			headers: { foo: 'bar' },
+			url: '/foo',
+			body: POST_DATA,
+		});
+
+		expect(mockClient.history.post.length).toBe(1);
+		expect(data).toMatchObject(RESPONSE_DATA);
+		expect(status).toEqual(200);
+	});
+
+	it('expired token can be refreshed', async () => {
+		const credentials = {
+			client_id: 'client',
+			client_secret: 'secret',
+			access_token: 'access_token',
+			refresh_token: 'refresh_token',
+		};
+		authService.getCredentials = async () => credentials;
+		authService.refreshToken = async () => {
+			credentials.access_token = 'new_access_token';
+			credentials.refresh_token = 'new_refresh_token';
+			return credentials;
+		};
+
+		const POST_DATA = {
+			hello: 'world',
+		};
+		mockClient
+			.onPost(`${API_ENDPOINT}/foo`, POST_DATA)
+			.replyOnce(TOKEN_EXPIRED_STATUS)
+			.onPost(`${API_ENDPOINT}/foo`, POST_DATA)
+			.reply(200);
+
+		await api.request({
+			method: 'post',
+			url: '/foo',
+			body: POST_DATA,
+		});
+
+		expect(mockClient.history.post.length).toBe(2);
+		expect(mockClient.history.post[0].headers).toMatchObject({
+			['x-amz-access-token']: 'access_token',
+		});
+		expect(mockClient.history.post[1].headers).toMatchObject({
+			['x-amz-access-token']: 'new_access_token',
+		});
+	});
+
+	it('failed refresh request not infinity looping', async () => {
+		authService.getCredentials = async () => {
+			Promise.resolve();
+			return {
+				access_token: 'token',
+			};
+		};
+
+		mockClient.onPost(`${API_ENDPOINT}/foo`).reply(TOKEN_EXPIRED_STATUS);
+		await api
+			.request({
+				method: 'post',
+				url: '/foo',
+			})
+			.catch((err) => {
+				expect(err instanceof AxiosError);
+				expect(err.response.status).toEqual(TOKEN_EXPIRED_STATUS);
+			});
+
+		expect(mockClient.history.post.length).toEqual(2);
+	});
+});
+
+describe('db errors', () => {
+	it('request', async () => {
+		authService.getCredentials = () => {
+			throw new Error('DB Error');
+		};
+
+		mockClient.onPost().reply(200);
+
+		await api
+			.request({
+				method: 'post',
+				url: '/foo',
+			})
+			.catch((err) => {
+				expect(err).toBeDefined();
+				expect(err instanceof Error).toBe(true);
+			});
+
+		expect(mockClient.history.post.length).toBe(0);
+	});
+});
